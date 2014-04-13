4578208e9aad82aef7cfcb5a7c897baa43f1faff
Anna <anna.yanova@gmail.com>
2014-04-13 21:16:48 +0100

    Question 2 of 39

--- questions.rb
+++ questions.rb
@@ -5,7 +5,7 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-
+  array.select { |e| e.start_with?("a", "e", "i", "o", "u") }
 end
 
 # remove instances of nil (but NOT false) from an array
--- questions_spec.rb
+++ questions_spec.rb
@@ -13,7 +13,7 @@ describe 'the Friday test :)' do
     expect(n).to eq ['apples', 'avocados']
   end
 
-  specify 'select_elements_starting_with_vowel' do
+  fit 'select_elements_starting_with_vowel' do
     n = select_elements_starting_with_vowel ['john', 'david', 'omar', 'fred', 'idris', 'angela']
     expect(n).to eq ['omar', 'idris', 'angela']
   end
