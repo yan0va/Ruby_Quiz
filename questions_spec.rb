8e653ac6dcffb468e9f63519f574599a5f8763b4
Anna <anna.yanova@gmail.com>
2014-04-13 22:40:54 +0100

    Question 6 of 39

--- questions_spec.rb
+++ questions_spec.rb
@@ -33,7 +33,7 @@ describe 'the Friday test :)' do
     expect(n).to eq ['god', 'yeknom', 'tnahpele']
   end
 
-  specify 'every_possible_pairing_of_students' do
+  fit 'every_possible_pairing_of_students' do
     n = every_possible_pairing_of_students(['Bob', 'Dave', 'Clive']) || []
     sorted = n.map {|pair| pair.sort}.sort_by {|pair| [pair.first, pair.last] }
 
