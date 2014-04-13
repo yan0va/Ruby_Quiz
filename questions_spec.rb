69277658e6ad194977ffad0d7b736f824d195312
Anna <anna.yanova@gmail.com>
2014-04-13 21:23:59 +0100

    Question 3 of 39

--- questions_spec.rb
+++ questions_spec.rb
@@ -18,7 +18,7 @@ describe 'the Friday test :)' do
     expect(n).to eq ['omar', 'idris', 'angela']
   end
 
-  specify 'remove_nils_from_array' do
+  fit 'remove_nils_from_array' do
     n = remove_nils_from_array ['a', 'b', nil, nil, false, 'c', nil]
     expect(n).to eq ['a', 'b', false, 'c']
   end
