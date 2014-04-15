c7b9c6004e1ce3242409b9e56ef04ba0be28bb52
Anna <anna.yanova@gmail.com>
2014-04-15 18:44:02 +0100

    Question 27 of 39

--- questions_spec.rb
+++ questions_spec.rb
@@ -146,7 +146,7 @@ describe 'the Friday test :)' do
     expect(n).to eq 4
   end
 
-  specify 'round_down_number' do
+  fit 'round_down_number' do
     n = round_down_number 4.9
     expect(n).to eq 4
   end
