ab1e206e4c820eca945d0784e9f19bb2cc04d7f0
Anna <anna.yanova@gmail.com>
2014-04-15 19:17:22 +0100

    Question 29 of 39

--- questions.rb
+++ questions.rb
@@ -176,6 +176,7 @@ end
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  email[/(\w+)m./]
 end
 
 # capitalize the first letter in each word of a string, 
